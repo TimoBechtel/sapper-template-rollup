d56c68cfd78d3965eac1c3fff9e03fab657cbc99
Timo Bechtel <timo@bechtel.solutions>
2020-02-09 23:22:32 +0100

    feat(tailwindcss): add tailwindcss with purgecss

--- postcss.config.js
+++ postcss.config.js
@@ -1,3 +1,18 @@
+const purgecss = require('@fullhuman/postcss-purgecss')({
+  content: ['./src/**/*.html', './src/**/*.svelte'],
+
+  whitelistPatterns: [/svelte-/],
+
+  defaultExtractor: content => content.match(/[A-Za-z0-9-_:/]+/g) || [],
+});
+
+const production = process.env.NODE_ENV === 'production';
+
 module.exports = {
-  plugins: [require('postcss-import'), require('autoprefixer')],
+  plugins: [
+    require('postcss-import'),
+    require('tailwindcss'),
+    require('autoprefixer'),
+    ...(production ? [purgecss] : []),
+  ],
 };
